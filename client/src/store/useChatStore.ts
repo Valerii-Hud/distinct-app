3f658cc2cc0487af5f320f2b225c203662a111ea
Valerii Hud <support@valerii-hud.com>
2026-01-27 12:16:43 +0100

    optimize(client): created loadResource function to optimize code and remove duplication logic

--- client/src/store/useChatStore.ts
+++ client/src/store/useChatStore.ts
@@ -4,6 +4,14 @@ import { create } from 'zustand';
 import axiosInstance from '../lib/axios';
 import type { User } from '../types';
 
+type DataState = 'users' | 'messages';
+type LoadingState = 'isUsersLoading' | 'isMessagesLoading';
+interface LoadResource {
+  loadingState: LoadingState;
+  dataState: DataState;
+  endpoint: string;
+}
+
 interface ChatStore {
   messages: [];
   users: User[] | [];
@@ -13,12 +21,13 @@ interface ChatStore {
   isUsersLoading: boolean;
   isMessagesLoading: boolean;
 
+  loadResource: ({ loadingState, endpoint, dataState }: LoadResource) => void;
   getUsers: () => void;
   getMessage: (userId: User['_id']) => void;
   setSelectedUser: (userId: User['_id']) => void;
 }
 
-const useChatStore = create<ChatStore>((set) => ({
+const useChatStore = create<ChatStore>((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -27,33 +36,56 @@ const useChatStore = create<ChatStore>((set) => ({
   isUsersLoading: false,
   isMessagesLoading: false,
 
-  getUsers: async () => {
-    set({ isUsersLoading: true });
+  loadResource: async ({ loadingState, endpoint, dataState }) => {
+    set({ [`${loadingState}`]: true });
     try {
-      const response = await axiosInstance.get('/messages/users');
-      set({ users: response.data });
+      const response = await axiosInstance.get(endpoint);
+      set({ [`${dataState}`]: response.data });
     } catch (error) {
       if (error instanceof AxiosError) {
         toast.error(error.response?.data.message || 'Something went wrong');
       }
     } finally {
-      set({ isUsersLoading: false });
+      set({ [`${loadingState}`]: false });
     }
   },
 
-  getMessage: async (userId) => {
-    set({ isMessagesLoading: true });
+  getUsers: async () => {
+    get().loadResource({
+      loadingState: 'isUsersLoading',
+      dataState: 'users',
+      endpoint: '/messages/users',
+    });
+    // set({ isUsersLoading: true });
+    // try {
+    //   const response = await axiosInstance.get('/messages/users');
+    //   set({ users: response.data });
+    // } catch (error) {
+    //   if (error instanceof AxiosError) {
+    //     toast.error(error.response?.data.message || 'Something went wrong');
+    //   }
+    // } finally {
+    //   set({ isUsersLoading: false });
+    // }
+  },
 
-    try {
-      const response = await axiosInstance.get(`/messages/${userId}`);
-      set({ messages: response.data });
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        toast.error(error.response?.data.message || 'Something went wrong');
-      }
-    } finally {
-      set({ isMessagesLoading: false });
-    }
+  getMessage: async (userId) => {
+    get().loadResource({
+      loadingState: 'isMessagesLoading',
+      dataState: 'messages',
+      endpoint: `/messages/${userId}`,
+    });
+    // set({ isMessagesLoading: true });
+    // try {
+    //   const response = await axiosInstance.get(`/messages/${userId}`);
+    //   set({ messages: response.data });
+    // } catch (error) {
+    //   if (error instanceof AxiosError) {
+    //     toast.error(error.response?.data.message || 'Something went wrong');
+    //   }
+    // } finally {
+    //   set({ isMessagesLoading: false });
+    // }
   },
 
   setSelectedUser: (userId) => set({ selectedUser: userId }),
